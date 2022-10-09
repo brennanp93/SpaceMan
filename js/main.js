6a6915cf86fee827984360af975f6531c724f68f
brennanp93 <brennan.p.perez@gmail.com>
2022-10-08 18:24:37 -0700

    Saturday Evening CSS

--- js/main.js
+++ js/main.js
@@ -1,13 +1,18 @@
   /*----- constants -----*/
-
-
+const WORD_BANK = ['CRATE','SLANT', 'TRACE', 'ROAST', 'BEANS', 'ADIEU' ]; 
+const MAX_GUESSES = 5;
+const LETTERS = 
   /*----- state variables -----*/
-
+let winner;
+let
 
   /*----- cached elements  -----*/
-
+const resetButton = document.querySelector('button');
+const gameMessage = document.getElementById('message');
+const blankSpace = document.getElementById('guess');
 
   /*----- event listeners -----*/
-
+document.getElementById('keyboard').addEventListener('click', playerChoice);
+document.querySelector('button').addEventListener('click', resetGame)
 
   /*----- functions -----*/
