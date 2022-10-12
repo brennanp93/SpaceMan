69bf81d561ac33da7b61f56ee69bf0847c9e9d95
brennanp93 <brennan.p.perez@gmail.com>
2022-10-12 11:47:11 -0700

    Update Js with Mario. Figured out the image path.

--- js/main.js
+++ js/main.js
@@ -1,194 +1,73 @@
-  /*----- constants -----*/
-//word bank that will be randomized and be the secret word. 
-const WORD_BANK = ['CRATE','SLANT', 'TRACE', 'ROAST', 'BEANS', 'ADIEU' ]; 
-//number of guesses that the player gets
-const MAX_GUESSES = 5;
-// do I need to include the entire alphabet to run through? 
-// const LETTERS = ['a'];
-  /*----- state variables -----*/
-
-let blankSpaces;
-
-let solutionWord; //randomly generated word is held here. 
-
-let remainingTries; // how many tries left
-
-let letter; //letter of word
-
-let result; //if you guessed the word before tries runs out
-
-// let selectedLetters = []; // the player choice goes in here 
-
-
-  /*----- cached elements  -----*/
-// const letterSelect = document.getElementById('keyboard');
-const keyboardLetters = document.querySelectorAll('keyboard > p');
-const playAgainButton = document.querySelector('button');
-const gameMessage = document.getElementById('message');
-const wordEl = document.getElementById('guess');
-// V -- could this be just 'space 1' since the field is only one element?
-// const blankSpaces = document.getElementById('guess');
-
-  /*----- event listeners -----*/
-// document.getElementById('keyboard').addEventListener('click', playerChoice);
-playAgainButton.addEventListener('click', resetGame);
-// letterSelect.addEventListener('click', playerChoice);
-// document.querySelector('button').addEventListener('click', resetGame)
-
-  /*----- functions -----*/
-
-function render() {
-    // renderSolution();
-
-}
-  
-  
-// init();
-  
-// function init() {
-//     remainingTries = 5;
-//     solutionWord = randomWord();
-//     SelectedLetters = [];
-//     solutionWord.forEach(function() {
-//         let letterEl = document.createElement('div');
-//         wordEl.appendChild(letterEl);
-//     })
-//     render();
-// }
-    
-// function playerChoice(event) {
-//     if (event.target.tagName !== 'P' ||
-//     SelectedLetters.includes(event.target.innerText)
-//     ) return;
-//     SelectedLetters.push(event.target.innerText)
-//     if (solutionWord.includes(event.target.innerText)) {
-//         render();
-//     } else {
-//         remainingTries -= 1;
-//         render();
-//     }
-// };
-    
-// function renderSolution() {
-//     let letterEls = [...document.querySelectorAll('#guess > div')];
-//     solutionWord.forEach(function(letter, idx) {
-//         if (SelectedLetters.includes(letter)) {
-//             letterEls[idx].innerHTML = letter;
-//         }
-//     })
-// };
-
-function checkLetter() {
-
-}
-
-// function renderWord() {
-//     let letterEls = [...document.querySelectorAll(‘#word > div’)];
-//     word.forEach(function(letter, idx) {
-//         if (userChoice.includes(letter)) {
-//             letterEls[idx].innerHTML = letter;
-//         }
-//     })
-// }
-
-
-
-
-function randomWord() {
-   let wordIdx = Math.floor(Math.random() * WORD_BANK.length)
-   solutionWord = WORD_BANK[wordIdx].split('');
-   return solutionWord;
-};
-
-
-
-
-
-
-//This is moving the innertext of the click to the userChoiceArray
-// function playerChoice(event) {
-//     // userChoice = (event.target).innerText;
-//     if(event.target.tagName !== 'P') return;
-//     // userChoice[0] = event.target.innerText;
-//     userChoice.push(event.target.innerText);
-// }
-// use this in determining if there is a winner later
-// if(userChoice.length > MAX_GUESSES) return "you lose";
-
-
-        
-        //This is done
-        // this generates a random word from the WORD_BANK at the start of game.
-        // Then it splits it 
-
-        
-        
-        function resetGame() {
-            blankSpaces = ['null','null','null', 'null', 'null',];
-            SelectedLetters = ['null','null','null', 'null', 'null',];
-            solutionWord = randomWord();
-            // render();
-        }
-        
-        // function randomWord() {
-            //    let wordIdx = Math.floor(Math.random() * WORD_BANK.length)
-            //    currentWord = WORD_BANK[wordIdx];
-//    return currentWord.split('');
-// };
-
-
-
-
-//    This function will take the letter selected by the player and 
-// check it against the currentWord. 
-
-// we need to loop through the "currentWord" variable and see if it matches the letter that the player selected. 
-
-// function renderBoard() {
-// this function will take the information from playerChoice and update
-// the board accordingly. 
-// 1 add letter to blank space. 2remove piece of spaceman. 
-// 3 grey out letter on keyboard. 
-// }
-
-// --------------------------------------------------------------------
 
 const WORD_BANK1 = ['CRATES','BACKED', 'TRACES', 'BARELY', 'CLOSER', 'CLIENT' ]; 
 
-// let correctGuessLetters = [];
-let wrongGuessLetters = [];
-let correctGuessLetters = [];
-// const imagePath = `images/spaceman-0${wrongGuessLetters.length}.png`;
+let wrongGuessLetters;
 
-let remainingGuesses = 6
-let imagePath = `images/spaceman-0${remainingGuesses}.png`;
+let correctGuessLetters;
 
-let currentGuess;
 
-let solution = [];
+let remainingGuesses = 5;
 
-let selectedLetters = [];
+
+// let currentGuess;
+
+let solution;
+
+//This will determine win logic. Value of Null, W for win or L for loss
+let gameStatus;
+
+// let selectedLetters;
 
 // let solutionArray = solution.split('');
 
 
 
-//cached elements
-const correctLetter = document.querySelectorAll('guess > div');
-const spaceManImage = document.getElementById("spaceman");
-const inGameMessage = document.getElementById('livesLeft');
 
+//cached elements
+const correctLetter = document.getElementById('guess');
+const spaceManImage = document.querySelector("img");
+const livesLeft = document.getElementById('livesLeft');
+const wordEl = document.getElementById('guess');
+const playButton = document.getElementById('btn');
+const gameMessage = document.getElementById('message');
 // const removeObject = document.getElementById('spaceman5');
 // document.getElementById('keyboard').addEventListener('click', playTurn);
 document.getElementById('keyboard').addEventListener('click', playerLetterSelect);
-
+playButton.addEventListener('click', initialize);
 // functions
 // randomWordGenerator();
 initialize();
 
+function initialize() {
+    wrongGuessLetters = [];
+    gameStatus = null;
+    randomWordGenerator();
+    render();
+}
+
+function randomWordGenerator() {
+    let wordIndex = Math.floor(Math.random() * WORD_BANK1.length)
+    solution = WORD_BANK1[wordIndex].split('');
+    correctGuessLetters = solution.map(ltr => ltr === ' ' ? ' ' : '_' )
+    return correctGuessLetters;
+ };
+
+ function render() {
+     renderImage();
+     renderMessage();
+     correctLetter.textContent = correctGuessLetters.join('');
+     
+ }
+
+ function renderImage() {
+    const imagePath = `images/spaceman-0${wrongGuessLetters.length}.png`; 
+    spaceManImage.src = imagePath;
+ }
+
 function playerLetterSelect(event) {
     if(event.target.tagName !== 'P') return;
-    currentGuess = event.target.innerText;
+    currentGuess = event.target.textContent;
+    console.log(currentGuess)
     // if (selectedLetters.includes(currentGuess)) return;
     let correctGuess = isGuessInSolution(currentGuess);
     if(correctGuess === true) {
@@ -197,24 +76,29 @@ function playerLetterSelect(event) {
         console.log(correctGuessLetters)
         changeLetterSquareGreen(event.target);
         // display correct letter
-        displayCorrectLetter(event.target);
+        // displayCorrectLetter(event.target);
     } else { 
         // what happens if it is not correct?
         wrongGuessLetters.push(event.target.innerText);
         console.log(wrongGuessLetters)
         changeLetterSquareGrey(event.target);
-        remainingGuesses--;
+        // remainingGuesses--;
     }
+    gameStatus = getGameStatus();
     render()
 }
 
-function render() {
-    renderImage()
+function getGameStatus() {
+    if (!correctGuessLetters.includes('_')) return 'W'; 
+    if (wrongGuessLetters.length > remainingGuesses) return 'L';
+    return null;
 }
 
-function renderImage() {
-    spaceManImage.innerHTML = `<img src="images/spaceman-0${remainingGuesses}.png">`
+function isGuessInSolution(letter) {
+    return solution.includes(letter);
 }
+
+
 
 function displayCorrectLetter(target) {
     correctLetter.innerText = currentGuess;
@@ -235,24 +119,17 @@ function removeSpacemanLimb() {
     imagePath
 }
 
-function isGuessInSolution(letter) {
-    return solution.includes(letter);
+function renderMessage() {
+    if (gameStatus === 'W') {
+    gameMessage.innerText = `Congratulations!!`
+    } else if (gameStatus === 'L') {
+    gameMessage.innerText = `Oh no! You Lost! The secret word was ${solution.join('')}`
+    } else { 
+    gameMessage.innerText = `Keep Playing! You have ${remainingGuesses - wrongGuessLetters.length + 1} guesses left `
+
+    }
 }
 
-function randomWordGenerator() {
-    let wordIndex = Math.floor(Math.random() * WORD_BANK1.length)
-    solution = WORD_BANK1[wordIndex].split('');
-    return solution;
- };
-
-function initialize() {
-    solution = randomWordGenerator();
-    solution.forEach(function() {
-        let letterEl = document.createElement('div');
-        wordEl.appendChild(letterEl);
-    })
-    render();
-}
 
 // function renderSolutionv2(target) {
 //     //pushes the correctly selected letter to a new div element. 
