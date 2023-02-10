619c20468595ba0258cb2da2e1a2e6dd06d37528
brennanp93 <brennan.p.perez@gmail.com>
2023-02-10 14:56:56 -0800

    Add media query for mobile use. Update keyboard.

--- js/main.js
+++ js/main.js
@@ -29,90 +29,90 @@ playButton.addEventListener('click', refreshBoard);
 initialize();
 
 function initialize() {
-    wrongGuessLetters = [];
-    correctGuessLetters = [];
-    gameStatus = null;
-    remainingGuesses = 5;
-    playButton.style.visibility = 'hidden';
-    randomWordGenerator();
-    render();
+  wrongGuessLetters = [];
+  correctGuessLetters = [];
+  gameStatus = null;
+  remainingGuesses = 5;
+  playButton.style.visibility = 'hidden';
+  randomWordGenerator();
+  render();
 };
 
 function randomWordGenerator() {
-    let wordIndex = Math.floor(Math.random() * WORD_BANK.length)
-    solutionWord = WORD_BANK[wordIndex].split('');
-    correctGuessLetters = solutionWord.map(ltr => ltr === ' ' ? ' ' : '_');
+  let wordIndex = Math.floor(Math.random() * WORD_BANK.length)
+  solutionWord = WORD_BANK[wordIndex].split('');
+  correctGuessLetters = solutionWord.map(ltr => ltr === ' ' ? ' ' : '_');
 };
 
 function render() {
-    renderImage();
-    renderMessage();
-    correctLetter.innerHTML = correctGuessLetters.join('');
+  renderImage();
+  renderMessage();
+  correctLetter.innerHTML = correctGuessLetters.join('');
 };
 
 function renderImage() {
-    const imagePath = `images/spaceman-0${wrongGuessLetters.length}.png`;
-    spaceManImage.src = imagePath;
+  const imagePath = `images/spaceman-0${wrongGuessLetters.length}.png`;
+  spaceManImage.src = imagePath;
 };
 
 function handleDrop(event) {
-    if (event.target.tagName !== 'P') return;
-    if (gameStatus !== null) return;
-    currentGuess = event.target.textContent;
-    let correctGuess = isGuessInSolution(currentGuess);
-    if (correctGuess === true) {
-        displayCorrectLetter();
-        markLetterSquareCorrect(event.target);
-    } else {
-        wrongGuessLetters.push(event.target.innerText);
-        markLetterSquareIncorrect(event.target);
-        event.target.style.pointerEvents = 'none';
-    }
-    gameStatus = getGameStatus();
-    render()
+  if (event.target.tagName !== 'P') return;
+  if (gameStatus !== null) return;
+  currentGuess = event.target.textContent;
+  let correctGuess = isGuessInSolution(currentGuess);
+  if (correctGuess === true) {
+    displayCorrectLetter();
+    markLetterSquareCorrect(event.target);
+  } else {
+    wrongGuessLetters.push(event.target.innerText);
+    markLetterSquareIncorrect(event.target);
+    event.target.style.pointerEvents = 'none';
+  }
+  gameStatus = getGameStatus();
+  render()
 };
 
 function getGameStatus() {
-    if (!correctGuessLetters.includes('_')) return 'W';
-    if (wrongGuessLetters.length > remainingGuesses) return 'L';
-    return null;
+  if (!correctGuessLetters.includes('_')) return 'W';
+  if (wrongGuessLetters.length > remainingGuesses) return 'L';
+  return null;
 };
 
 function isGuessInSolution(letter) {
-    return solutionWord.includes(letter);
+  return solutionWord.includes(letter);
 };
 
 function markLetterSquareCorrect(target) {
-    target.style.backgroundColor = 'limegreen';
-    target.style.color = 'black';
+  target.style.backgroundColor = 'limegreen';
+  target.style.color = 'black';
 };
 
 function markLetterSquareIncorrect(target) {
-    target.innerText = '*'
-    target.style.color = 'red';
+  target.innerText = '*'
+  target.style.color = 'red';
 };
 
 function renderMessage() {
-    if (gameStatus === 'W') {
-        gameMessage.innerText = `Congratulations!! You saved the spaceman!`
-        playButton.style.visibility = 'visible';
-    } else if (gameStatus === 'L') {
-        gameMessage.innerText = `Oh no! You Lost! The secret word was ${solutionWord.join('')}`
-        playButton.style.visibility = 'visible';
-    } else {
-        gameMessage.innerText = `You have ${remainingGuesses - wrongGuessLetters.length + 1} guesses left `
+  if (gameStatus === 'W') {
+    gameMessage.innerText = `Congratulations!! You saved the spaceman!`
+    playButton.style.visibility = 'visible';
+  } else if (gameStatus === 'L') {
+    gameMessage.innerText = `Oh no! You Lost! The secret word was ${solutionWord.join('')}`
+    playButton.style.visibility = 'visible';
+  } else {
+    gameMessage.innerText = `You have ${remainingGuesses - wrongGuessLetters.length + 1} guesses left `
 
-    }
+  }
 };
 
 function displayCorrectLetter() {
-    solutionWord.forEach(function(letter, index) {
-        if (currentGuess === letter) {
-            correctGuessLetters[index] = currentGuess;
-        } 
-    })
+  solutionWord.forEach(function (letter, index) {
+    if (currentGuess === letter) {
+      correctGuessLetters[index] = currentGuess;
+    }
+  })
 }
 
 function refreshBoard() {
-    return window.location.reload()
+  return window.location.reload()
 };
